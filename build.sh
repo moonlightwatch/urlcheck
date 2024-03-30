697699403f6f935b046480e60724b06b4360fa70
moonlightwatch <moonlightwatch@hotmail.com>
2024-03-30 09:30:35 +0000

    update

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 
 go build -o ./bin/urlcheck ./
 CGO_ENABLED=0 GOOS=windows  GOARCH=amd64 go build -o ./bin/urlcheck.exe ./
+
+cp /usr/local/go/misc/wasm/wasm_exec.js bin/
+GOOS=js GOARCH=wasm go build -ldflags '-s -w' -o bin/main.wasm ./
